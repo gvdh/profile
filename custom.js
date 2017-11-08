b13d6a8fd23cca31b24add3b9505a1b1a7e2ad58
G VDH <vanderhaegen.gauthier@gmail.com>
2017-11-08 17:46:42 +0100

    change on id

--- custom.js
+++ custom.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    $('#example tr').click(function() {
+    $('#clickable tr').click(function() {
         var href = $(this).find("a").attr("href");
         if(href) {
             window.location = href;
