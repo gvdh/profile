a2eb3cfbfb05494bd2c692c85e9235dfb1a7a74b
G VDH <vanderhaegen.gauthier@gmail.com>
2017-11-08 17:40:05 +0100

    my profile page

---
+++ custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+    $('#example tr').click(function() {
+        var href = $(this).find("a").attr("href");
+        if(href) {
+            window.location = href;
+        }
+    });
+});
